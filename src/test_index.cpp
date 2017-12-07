7074748251286f1d7e0400874b2c6aa5514c34cc
Guillaume Desforges <guillaume.desforges@eleves.enpc.fr>
2017-12-07 21:27:32 +0100

    Begin test index

---
+++ src/test_index.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+#include "img/image.h"
+#include "clustering/kmeanstree.h"
+
+int main(int argc, char** argv )
+{
+    return 0;
+}
