cf43fefd9d8d5114fba317a8e9120a6a82ffa62e
Mailys Rheins <ms.rheins@gmail.com>
2017-12-07 23:41:12 +0100

    Added database

--- src/test_index.cpp
+++ src/test_index.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <experimental/filesystem>
+namespace fs = filesystem;
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
@@ -6,7 +8,35 @@
 #include "img/image.h"
 #include "clustering/kmeanstree.h"
 
+
+namespace fs = boost::filesystem;
+
+
+void create_database(string path_to_imagesFolder){
+  //map<int, Image*> database;
+  //int k=0;
+
+  fs::path path_to_imFolder(path_to_imagesFolder);
+  fs::directory_iterator end_iter;
+
+  typedef std::multimap<std::time_t, fs::path> result_set_t;
+  result_set_t result_set;
+
+  if ( fs::exists(someDir) && fs::is_directory(someDir)){
+    for( fs::directory_iterator dir_iter(someDir) ; dir_iter != end_iter ; ++dir_iter){
+      if (fs::is_regular_file(dir_iter->status()) ){
+        result_set.insert(result_set_t::value_type(fs::last_write_time(dir_iter->path()), *dir_iter));
+      }
+    }
+  }
+  //Image * image(p);
+  //database.insert(pair<int,Image*>(k,image));
+}
+
+
+
 int main(int argc, char** argv )
 {
+
     return 0;
 }
